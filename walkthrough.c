dfae604b15b8591a7667e868d636c3d63a013ac8
Kahjax <kj1124@msstate.edu>
2022-03-31 19:22:19 -0500

    Changed the file type to c

---
+++ walkthrough.c
@@ -0,0 +1,30 @@
+// **************************************
+//  Guide to Codechat
+// **************************************
+// -------------
+// Introduction
+// -------------
+// This is a walkthrough guide to explain what you will need to know in order to succeed in learning codechat. Once you have read and understood this section there will be an interactive jeopardy game located on the home
+//
+// --------------------------
+// How to install Code Chat 
+// --------------------------
+//   *The steps on downloading the Code Chat software 
+//   *A section on how the command prompt is used to download //the software*
+// --------------------------
+// Basic Code Chat Functions 
+// --------------------------
+//  
+// --------------------------
+// How to install Code Chat 
+// --------------------------
+// --------------------------
+// How to install Code Chat 
+// --------------------------
+// So what do I have to do?
+// ====================================
+// .. image:: image name
+//   :width: 100%
+//
+// 
+// I like cheese

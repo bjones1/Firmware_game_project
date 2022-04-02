d58377f7e06cf116f252ac363d150dd4b0129a7f
Kahjax <kj1124@msstate.edu>
2022-04-02 15:59:20 -0500

    Added some new features and modified walkthrough

---
+++ ppf.c
@@ -0,0 +1,33 @@
+// *******************************
+// **Past, Present, and Future**
+// *******************************
+/*
+*Past*
+------------
+**Development**
+=================
+We came up with the concept to build this website because we recognized a need for a single textbook that had all of the links and material for this course. Because this is the first semester that this course has been offered, students may find it challenging to grasp some aspects of the class at first, such as the format and spacing in the code chat software. This guide was intended to assist you in navigating the class materials and serves as a general overview of everything you will learn.
+
+*Present*
+----------------------
+**Format of this E-book**
+==========================
+*  The walkthrough html page that explains the information that will be covered in the codechat course (insert link)
+*   The jeopardy game interactive game that will test your knowledge of what you read in the walkthrough
+
+Future
+------------------
+**End Goal**
+===============
+Thor will move on this map that has the coordinates of 40 wide by 18 high. As the light of power changes locations your goal is change the direction of Thor's movements based on the coordinate placement of the light of power.
+
+
+*Note: You can utilize any coding language that you are most comfortable with to write this code by changing this option in the code tab. I chose to use C++*
+
+Authors & Contributors
+----------------------
+In constructing this E-book and interactive gameplay these are the designers and contributors to this website.
+
+*   `Kaitlyn Jackson <https://github.com/Kahjax>`_
+*   Olivia Starnes
+*/ 
--- walkthrough.c
+++ walkthrough.c
@@ -1,30 +1,21 @@
-// **************************************
-//  Guide to Codechat
-// **************************************
-// -------------
-// Introduction
-// -------------
-// This is a walkthrough guide to explain what you will need to know in order to succeed in learning codechat. Once you have read and understood this section there will be an interactive jeopardy game located on the home
-//
-// --------------------------
-// How to install Code Chat 
-// --------------------------
-//   *The steps on downloading the Code Chat software 
-//   *A section on how the command prompt is used to download //the software*
-// --------------------------
-// Basic Code Chat Functions 
-// --------------------------
-//  
-// --------------------------
-// How to install Code Chat 
-// --------------------------
-// --------------------------
-// How to install Code Chat 
-// --------------------------
-// So what do I have to do?
-// ====================================
-// .. image:: image name
-//   :width: 100%
-//
-// 
-// I like cheese
+// ***********************************
+// **Walkthrough Guide for Codechat**
+// ***********************************
+/*
+This is a walkthrough guide to explain what you will need to know in order to succeed in learning codechat. Once you have read and understood this section there will be an interactive `Jeopardy <insert link>`_ game located on the homepage.
+
+How to install Code Chat
+--------------------------
+Use `this link <https://codechat-system.readthedocs.io/en/latest/CodeChat_Server/install.html>`_ to navigate to the installation homepage 
+
+History of Codechat
+---------------------
+
+
+Code Chat Basics
+------------------
+**Formatting**
+===============
+Thor will move on this map that has the coordinates of 40 wide by 18 high. As the light of power changes locations your goal is change the direction of Thor's movements based on the coordinate placement of the light of power.
+
+*/
